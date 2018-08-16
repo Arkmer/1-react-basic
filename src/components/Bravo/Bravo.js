1b14bbdf568e093749178d3fff7b9f71016cd723
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-08-15 21:15:13 -0500

    building lists, break to learn JS map

--- src/components/Bravo/Bravo.js
+++ src/components/Bravo/Bravo.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import './Bravo.css';
 
 class Bravo extends Component {
+
+  constructor(props) {
+    super(props); // Do this or things won't work right
+      this.state = {
+        starList: ['hadar', 'gacrux', 'fomalhaut'],
+      }
+    }
   
     render() {
+      let starListItemArray = [];
+      for(let i = 0; i < this.state.starList.length; i++){
+        let starListItem = <li>{ this.state.starList[i] }</li>;
+        starListItemArray.push(starListItem);
+      }
       return (
         <div>
             <h1>Bravo Page</h1>
-            
+            <div>
+              this.state = { JSON.stringify(this.state) }
+            </div>
+            <div>
+              this.state.starList = { this.state.starList }
+            </div>
+            <ul>
+              { starListItemArray }
+            </ul>
         </div>
     );
   }
